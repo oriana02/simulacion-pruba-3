2fe60b1aad34fbfbccbcc277a74d5594ed0a5d29
oriana02 <or.solorzano@duocuc.cl>
2024-06-25 12:16:54 -0400

    registrar pedido

--- simulacion prueba 3.py
+++ simulacion prueba 3.py
@@ -3,5 +3,12 @@ def menu():
              2. Listas de todos los pedidos
              3. Imprimir hoja de ruta
              4. Salir del programa""")
-             
 
+def registrar_pedido(nombre, apellido, comuna, detalle_pedido ):
+    if nombre == "":
+        nombre = input("Ingrese Nombre: ")
+        apellido = input("Ingrese Apellido: ")
+        comuna = input("Ingrese comuna")
+        detalle_pedido =input("Detalle del pedido")
+        
+        
