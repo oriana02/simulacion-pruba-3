82b2c5ae162e0bc037e751386feeae49985b163e
oriana02 <or.solorzano@duocuc.cl>
2024-06-25 12:03:11 -0400

    Add files via upload

---
+++ simulacion prueba 3.py
@@ -0,0 +1,7 @@
+def menu():
+    print("""1. Registrar pedido
+             2. Listas de todos los pedidos
+             3. Imprimir hoja de ruta
+             4. Salir del programa""")
+             
+
